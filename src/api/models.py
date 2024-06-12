6bcb037059e3a46fcf7d488ff349d8e1a7a93794
Anais Jimenez <158285035+anaispj@users.noreply.github.com>
2024-06-12 19:33:18 +0000

    todo completo el models y probado

--- src/api/models.py
+++ src/api/models.py
@@ -1,9 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Enum  # Enum?
-
+from sqlalchemy import Enum
 
 db = SQLAlchemy()
-
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,7 +11,7 @@ class Users(db.Model):
     first_name = db.Column(db.String(), unique=False, nullable=False)
     last_name = db.Column(db.String(), unique=False, nullable=False)
     age = db.Column(db.Integer(), unique=False, nullable=False)
-    is_admin = db.Column(db.Boolean(80), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>' 
@@ -26,7 +24,6 @@ class Users(db.Model):
                 'last_name': self.last_name,
                 'age': self.age,
                 'is_admin': self.is_admin}
-    
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,13 +31,13 @@ class Posts(db.Model):
     body = db.Column(db.String(150), unique=False, nullable=False)
     date = db.Column(db.Date(), unique=False, nullable=True)
     image_url = db.Column(db.String(), unique=False, nullable=True)
-    author_id = db.Column(db.Integer, db,ForeignKey('users.id'))  # Hace falta poner nullable aqui?
-    author_to = db.relationship('Users, foreign_keys=[user_id]')
-    game_id = db.Column(db.Integer, db,ForeignKey('games.id')) 
-    game_to = db.relationship('Games, foreign_keys=[game_id]')
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    author_to = db.relationship('Users', foreign_keys=[author_id])
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id')) 
+    game_to = db.relationship('Games', foreign_keys=[game_id])
 
     def __repr__(self):
-        return f'<User {self.title}>' 
+        return f'<Post {self.title}>' 
 
     def serialize(self):
         return {'id': self.id,
@@ -49,130 +46,125 @@ class Posts(db.Model):
                 'date': self.date,
                 'image_url': self.image_url,
                 'author_id': self.author_id,
-                'game_id': self.games_id}
-    
+                'game_id': self.game_id}
 
 class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), unique=True, nullable=False)
-    body_img = db.Column(db.String(), unique=True)
+    body_img = db.Column(db.String(), unique=False, nullable=True) # Modificación aquí
     cdk = db.Column(db.String(), unique=True, nullable=False)
     price = db.Column(db.Integer(), unique=False, nullable=False)
-    game_id = db.Column(db.Integer, db,ForeignKey('games.id')) 
-    game_to = db.relationship('Games, foreign_keys=[game_id]')
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id')) 
+    game_to = db.relationship('Games', foreign_keys=[game_id])
 
     def __repr__(self):
-        return f'<User {self.name}>' 
+        return f'<Product {self.name}>' 
 
     def serialize(self):
         return {'id': self.id,
                 'name': self.name,
-                'body': self.body,
+                'body_img': self.body_img, # Modificación aquí
                 'cdk': self.cdk,
                 'price': self.price,
                 'game_id': self.game_id}
-    
 
 class Likes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db,ForeignKey('games.id')) 
-    product_to = db.relationship('Games, foreign_keys=[product_id]')
-    user_id = db.Column(db.Integer, db,ForeignKey('users.id')) 
-    user_to = db.relationship('Users, foreign_keys=[user_id]')
-
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id')) 
+    product_to = db.relationship('Products', foreign_keys=[product_id])
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+    user_to = db.relationship('Users', foreign_keys=[user_id])
 
     def __repr__(self):
-        return f'<User {self.product_id}>'  # No sabemos bien aqui se aprecia asistencia muchas gracias
+        return f'<Like {self.id}>' 
 
     def serialize(self):
         return {'id': self.id,
                 'product_id': self.product_id,
                 'user_id': self.user_id}
 
-
 class Games(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(), unique=True, nullable=False)
-    image_url = db.Column(db.String(), unique=True)
+    image_url = db.Column(db.String(), unique=True, nullable=True) # Modificación aquí
     description = db.Column(db.String(), unique=False, nullable=False)
     genre = db.Column(db.String(), unique=False, nullable=False)
-    platform = db.Column(Enum('computer', 'playstation', 'xbox', 'switch', 'mobile', name='platform_enum'), unique=False, nullable=False)
+    # platform = db.Column(Enum('computer', 'playstation', 'xbox', 'switch', 'mobile', name='platform_enum'), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.title}>' 
+        return f'<Game {self.title}>' 
 
     def serialize(self):
         return {'id': self.id,
                 'title': self.title,
-                'image_url': self.image_url,
+                'image_url': self.image_url, # Modificación aquí
                 'description': self.description,
-                'genre': self.genre,
-                'platform': self.platform}
-
+                # 'platform': self.platform,
+                'genre': self.genre}
 
 class CarItems(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer(), unique=False, nullable=False)
     price = db.Column(db.Integer(), unique=False, nullable=False)
-    product_id = db.Column(db.Integer, db,ForeignKey('caritems.id')) 
-    product_to = db.relationship('CarItems, foreign_keys=[product_id]')
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id')) 
+    product_to = db.relationship('Products', foreign_keys=[product_id]) # Modificación aquí
 
     def __repr__(self):
-        return f'<User {self.products_id}>'  # No sabemos bien
+        return f'<CarItem {self.product_id}>' 
 
     def serialize(self):
         return {'id': self.id,
-                'product_id': self.products_id,
+                'product_id': self.product_id,
                 'quantity': self.quantity,
                 'price': self.price}
   
 
 class Carts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.Enum('en proceso', 'cancelado'), unique=False)
+    # status = db.Column(db.Enum('en proceso', 'cancelado'), unique=False)
     date = db.Column(db.Date(), unique=False, nullable=False)
-    user_id = db.Column(db.Integer, db,ForeignKey('users.id')) 
-    user_to = db.relationship('Users, foreign_keys=[user_id]')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+    user_to = db.relationship('Users', foreign_keys=[user_id]) # Modificación aquí
 
     def __repr__(self):
-        return f'<User {self.status}>' 
+        return f'<Cart {self.id}>' 
 
     def serialize(self):
         return {'id': self.id,
                 'user_id': self.user_id,
-                'status': self.status,
+                # 'status': self.status,
                 'date': self.date}
 
 class Orders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date(), unique=False, nullable=False)
     price_total = db.Column(db.Integer(), unique=False, nullable=False)
-    status = db.Column(db.Enum('pagado', 'cancelado', 'pendiente de pago'), unique=False)
-    user_id = db.Column(db.Integer, db,ForeignKey('users.id')) 
-    user_to = db.relationship('Users, foreign_keys=[user_id]')
+    # status = db.Column(db.Enum('pagado', 'cancelado', 'pendiente de pago'), unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+    user_to = db.relationship('Users', foreign_keys=[user_id]) # Modificación aquí
 
     def __repr__(self):
-        return f'<User {self.status}>' 
+        return f'<Order {self.id}>' 
 
     def serialize(self):
         return {'id': self.id,
-                'user_id': self.products_id,
-                'date': self.quantity,
-                'price_total': self.price_total,
-                'status': self.status}
+                'user_id': self.user_id,
+                'date': self.date,
+                # 'status': self.status,
+                'price_total': self.price_total}
 
 
 class OrderItems(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer(), unique=False, nullable=False)
     price = db.Column(db.Integer(), unique=False, nullable=False)
-    order_id = db.Column(db.Integer, db,ForeignKey('orders.id')) 
-    order_to = db.relationship('Orders, foreign_keys=[order_id]')
-    product_id = db.Column(db.Integer, db,ForeignKey('products.id')) 
-    product_to = db.relationship('Products, foreign_keys=[product_id]')
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id')) 
+    order_to = db.relationship('Orders', foreign_keys=[order_id])
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id')) 
+    product_to = db.relationship('Products', foreign_keys=[product_id])
 
     def __repr__(self):
-        return f'<User {self.order_id}>' 
+        return f'<OrderItem {self.id}>' 
 
     def serialize(self):
         return {'id': self.id,
@@ -180,5 +172,3 @@ class OrderItems(db.Model):
                 'product_id': self.product_id,
                 'quantity': self.quantity,
                 'price': self.price}
-
-
