4091d5a6f2beb1b39f31b48608a71da44317e168
Anais Jimenez <158285035+anaispj@users.noreply.github.com>
2024-06-12 17:56:19 +0000

    models - users

--- src/api/models.py
+++ src/api/models.py
@@ -9,11 +9,20 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(), unique=False, nullable=True)
+    last_name = db.Column(db.String(), unique=False, nullable=True)
+    age = db.Column(db.Integer(), unique=False, nullable=True)
+    is_admin = db.Column(db.Boolean(80), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.email}>' 
 
     def serialize(self):
         return {'id': self.id,
                 'email': self.email,
-                'is_active': self.is_active}
+                'is_active': self.is_active,
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'age': self.age,
+                'is_admin': self.is_admin}
+    
