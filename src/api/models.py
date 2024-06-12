e67fa6c96b8aec36db83364e56c4903123a6b7ca
Anais Jimenez <158285035+anaispj@users.noreply.github.com>
2024-06-12 18:23:20 +0000

    models - games

--- src/api/models.py
+++ src/api/models.py
@@ -9,9 +9,9 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    first_name = db.Column(db.String(), unique=False, nullable=True)
-    last_name = db.Column(db.String(), unique=False, nullable=True)
-    age = db.Column(db.Integer(), unique=False, nullable=True)
+    first_name = db.Column(db.String(), unique=False, nullable=False)
+    last_name = db.Column(db.String(), unique=False, nullable=False)
+    age = db.Column(db.Integer(), unique=False, nullable=False)
     is_admin = db.Column(db.Boolean(80), unique=False, nullable=False)
 
     def __repr__(self):
@@ -29,12 +29,12 @@ class Users(db.Model):
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(), unique=False, nullable=True)
-    body = db.Column(db.String(150), unique=False, nullable=True)
+    title = db.Column(db.String(), unique=False, nullable=False)
+    body = db.Column(db.String(150), unique=False, nullable=False)
     date = db.Column(db.Date(), unique=False, nullable=True)
     image_url = db.Column(db.String(), unique=False, nullable=True)
-    author_id = db.Column(db.Integer(), unique=True, nullable=True)
-    games_id = db.Column(db.Boolean(), unique=True, nullable=True)
+    author_id = db.Column(db.Integer(), unique=True)  # Hace falta poner nullable aqui?
+    games_id = db.Column(db.Boolean(), unique=True)
 
     def __repr__(self):
         return f'<User {self.title}>' 
@@ -51,11 +51,11 @@ class Posts(db.Model):
 
 class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), unique=True, nullable=True)
-    body_img = db.Column(db.String(), unique=True, nullable=True)
-    cdk = db.Column(db.String(), unique=True, nullable=True)
-    price = db.Column(db.Integer(), unique=False, nullable=True)
-    game_id = db.Column(db.Integer(), unique=True, nullable=False)
+    name = db.Column(db.String(), unique=True, nullable=False)
+    body_img = db.Column(db.String(), unique=True)
+    cdk = db.Column(db.String(), unique=True, nullable=False)
+    price = db.Column(db.Integer(), unique=False, nullable=False)
+    game_id = db.Column(db.Integer(), unique=True)
 
     def __repr__(self):
         return f'<User {self.name}>' 
@@ -82,8 +82,30 @@ class Likes(db.Model):
         return {'id': self.id,
                 'product_id': self.product_id,
                 'user_id': self.user_id}
-    
-    
+
+
+class Games(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), unique=True, nullable=False)
+    image_url = db.Column(db.String(), unique=True)
+    description = db.Column(db.String(), unique=False, nullable=False)
+    genre = db.Column(db.String(), unique=False, nullable=False)
+    platform = db.Column(db.Integer(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>' 
+
+    def serialize(self):
+        return {'id': self.id,
+                'title': self.title,
+                'image_url': self.image_url,
+                'description': self.description,
+                'genre': self.genre,
+                'platform': self.platform}
+
+
+
+  
 
  
 
