d43fc0c30dd1d4ac54b4b1cf0de28436e24dd3bf
Anais Jimenez <158285035+anaispj@users.noreply.github.com>
2024-06-20 13:58:08 +0000

    login

--- src/front/js/Layout.jsx
+++ src/front/js/Layout.jsx
@@ -10,6 +10,7 @@ import { Footer } from "./component/Footer.jsx";
 import { Home } from "./pages/Home.jsx";
 import { Demo } from "./pages/Demo.jsx";
 import { Single } from "./pages/Single.jsx";
+import { Login } from "./pages/Login.jsx";
 
 
 // Create your first component
@@ -30,6 +31,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Login />} path="/login" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
