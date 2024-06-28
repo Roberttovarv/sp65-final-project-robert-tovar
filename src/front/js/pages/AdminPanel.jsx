e8d8b192f33d08b4bf7ced8a803084786379e039
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-06-28 17:09:57 +0000

    para develop

--- src/front/js/pages/AdminPanel.jsx
+++ src/front/js/pages/AdminPanel.jsx
@@ -10,26 +10,26 @@ export const AdminPanel = () => {
     return (
         <>
             <div>
-            (
+                (
                 <div className="d-flex flex-column" style={{ height: "80vh" }}>
                     <div className="text-center my-3">
                         <h1>Panel de administrador</h1>
                     </div>
-<div className="container-fluid m-auto">
-                    <div className="row justify-content-around mx-5 align-items-center" style={{ height: "100%" }}>
-                        <div className="col-3">
-                        <Link to="/adminpanel/gamepanel"><button className="comic-button" style={{ height: "30vh", width: "20vw" }}>Crear Videojuego</button></Link>
+                    <div className="container-fluid m-auto">
+                        <div className="row justify-content-around mx-5 align-items-center" style={{ height: "100%" }}>
+                            <div className="col-3">
+                                <Link to="/adminpanel/gamepanel"><button className="comic-button" style={{ height: "30vh", width: "20vw" }}>Crear Videojuego</button></Link>
+                            </div>
+                            <div className="col-3">
+                                <button className="comic-button" style={{ height: "30vh", width: "20vw" }}>Crear Producto</button>
+                            </div>
+                            <div className="col-3">
+                                <button className="comic-button" style={{ height: "30vh", width: "20vw" }}>Crear Post</button>
+                            </div>
+                            <div className="col-3">
+                            <Link to="/adminpanel/userpanel"><button className="comic-button" style={{ height: "30vh", width: "20vw" }}>Ver Usuarios</button></Link>
+                            </div>
                         </div>
-                        <div className="col-3">
-                            <button className="comic-button" style={{ height: "30vh", width: "20vw" }}>Crear Producto</button>
-                        </div>
-                        <div className="col-3">
-                            <button className="comic-button" style={{ height: "30vh", width: "20vw" }}>Crear Post</button>
-                        </div>
-                        <div className="col-3">
-                            <button className="comic-button" style={{ height: "30vh", width: "20vw" }}>Ver Usuarios</button>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </div>
