f6c54d7d8777b66a2c1724f07897a359be305301
David <149296106+dmeleroandrade@users.noreply.github.com>
2024-07-01 19:18:55 +0000

    cambios api2 y todas imagenes iguales

--- src/front/js/pages/Tienda.jsx
+++ src/front/js/pages/Tienda.jsx
@@ -10,26 +10,49 @@ export const Tienda = () => {
 
     const host = `${process.env.BACKEND_URL}`
 
+    const postGames = async () => {
+
+        const uri = host + '/api/apiexterna';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                game_ids: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+            })
+        }
+
+        const response = await fetch(uri, options);
+
+
+        if (!response.ok) {
+            console.log("Error", response.status, response.statusText);
+        }
+
+
+    }
+
+    const getGames = async () => {
+
+        const uri = host + '/api/games';
+        const options = { method: 'GET' };
+
+        const response = await fetch(uri, options);
+
+        if (!response.ok) {
+            console.log("Error", response.status, response.statusText);
+        }
+
+        const data = await response.json();
+        setGames(data.results);
+    };
 
     useEffect(() => {
-      getGames();
+        getGames();
+        postGames();
     }, []);
-  
-    const getGames = async () => {
-      
-      const uri = host + '/api/games';
-      const options = { method: 'GET' };
-  
-      const response = await fetch(uri, options);
-  
-      if (!response.ok) {
-        console.log("Error", response.status, response.statusText);
-      }
-  
-      const data = await response.json();
-      setGames(data.results);
-    };
-  
+    console.log(games);
     return (
         <div className="container">
             <h1 className="text-center text-light">TIENDA</h1>
@@ -37,7 +60,7 @@ export const Tienda = () => {
                 {games.map((game, index) => (
                     <div key={index} className="col-md-4">
                         <div className="card mb-4 shadow-sm">
-                            <img src={game.background_image} className="card-img-top" alt={game.name} />
+                            <img src={game.background_image} className="card-img-top" width= "250" height='250' alt={game.name} />
                             <div className="card-body">
                                 <h5 className="card-title">{game.name}</h5>
                                 <p className="card-text">Rating: {game.rating}</p>
@@ -54,4 +77,4 @@ export const Tienda = () => {
             </div>
         </div>
     );
-};
+}
