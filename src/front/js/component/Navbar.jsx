eebb27c6be1aa52f65f29933735444d96a6181a2
Anais Jimenez <158285035+anaispj@users.noreply.github.com>
2024-07-03 17:00:12 +0000

    fixing navbar

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -6,7 +6,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 export const Navbar = () => {
-    const { store } = useContext(Context);
 
 	const { store, actions } = useContext(Context)
 
