39f242e15f6a587d38e7a8f1eb57a2bf02c5f2e6
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-07-23 12:09:45 +0200

    Create NotFound.jsx

---
+++ src/front/js/component/NotFound.jsx
@@ -0,0 +1,15 @@
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+
+import { useState } from "react";
+
+export const NotFound = () => {
+
+return (<>
+  <h1 className="text-center text-light">Page not found!</h1>
+<div className="d-flex m-0 justify-content-center">
+<img src="https://www.pngall.com/wp-content/uploads/13/Space-Invaders-PNG-Photos.png" alt="404 Not Found" />
+</div>
+</>
+)}
