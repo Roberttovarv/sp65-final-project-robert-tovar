cf607608b70c76c14206f1b52aa42181a98629d3
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-07-29 13:09:28 +0200

    Update Reviews.jsx

--- src/front/js/pages/Reviews.jsx
+++ src/front/js/pages/Reviews.jsx
@@ -1,32 +1,53 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const Reviews = () => {
-    const { store, actions } = useContext(Context);
+const { store, actions } = useContext(Context);
+const [video, setVideo] = useState([]);
+
+        const host = `${process.env.BACKEND_URL}`
+
+    const getVideos = async () => {
+        const uri = host + '/api/videos';
+        const options = { method: 'GET' };
+
+        const response = await fetch(uri, options);
+
+        if (!response.ok) {
+            console.log("Error", response.status, response.statusText);
+            return;
+        }
+
+        const data = await response.json();
+
+        setVideo(data);
+    };
 
     useEffect(() => {
+        getVideos();
           
     }, []);
 
 
     return (
         <div className="container">
-            {/* <h1 className="text-center text-light">Reseñas</h1>
-            <div className="row overflow-auto" style={{ maxHeight: '80vh' }}>
-                {reviews.map((review, index) => (
-                    <div key={index} className="card m-3" style={{ width: '18rem' }}>
-                        <div className="card-body">
-                            <h5 className="card-title">{review.title}</h5>
-                            <p className="card-text">{review.description}</p>
-                            <div className="d-flex justify-content-between">
-                                <Link to={'/review-details/' + review.id} className="btn btn-outline-secondary">Detalles</Link>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div> */}
-            Reseñas
+            {video.map((video, index) => (
+                <div key={index}>
+                    <h3 className="text-light">{video.title}</h3>
+                    <h6 className="text-light">{video.videogame}</h6>
+                    <iframe 
+                        width="560" 
+                        height="315" 
+                        src={video.embed}
+                        title="YouTube video player" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerPolicy="strict-origin-when-cross-origin" 
+                        allowFullScreen
+                    ></iframe>
+                </div>
+            ))}
         </div>
     );
 };
