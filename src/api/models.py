5b4268fc500bdade32c0d9957293a13fda351349
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-08-01 09:41:22 +0200

    Update models.py

--- src/api/models.py
+++ src/api/models.py
@@ -4,37 +4,37 @@ from flask import request, jsonify
 from datetime import datetime
 import re
 
-
 def delete_html_tags(text):
-        clean = re.compile('<.*?>')
-        return re.sub(clean, '', text)
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
 
 db = SQLAlchemy()
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=True)
-    first_name = db.Column(db.String(), unique=False, nullable= True)
-    last_name = db.Column(db.String(), unique=False, nullable= True)
+    first_name = db.Column(db.String(), unique=False, nullable=True)
+    last_name = db.Column(db.String(), unique=False, nullable=True)
     username = db.Column(db.String(), unique=True, nullable=False)
     is_admin = db.Column(db.Boolean(), unique=False, nullable=True)
     pfp = db.Column(db.String(), unique=False, nullable=True)
-
 
     def __repr__(self):
         return f'<User {self.email}>'
         
     def serialize(self):
-        return {'id': self.id,
-                'email': self.email,
-                'is_active': self.is_active,
-                'first_name': self.first_name,
-                'last_name': self.last_name,
-                'username': self.username,
-                'is_admin': self.is_admin,
-                'pfp': self.pfp}
-
+        return {
+            'id': self.id,
+            'email': self.email,
+            'is_active': self.is_active,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'username': self.username,
+            'is_admin': self.is_admin,
+            'pfp': self.pfp
+        }
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -43,7 +43,6 @@ class Posts(db.Model):
     body = db.Column(db.String(), unique=False, nullable=False)
     date = db.Column(db.Date(), unique=False, nullable=True)
     image_url = db.Column(db.String(), unique=False, nullable=True)
- 
 
     def __repr__(self):
         return f'<Post {self.title}>'
@@ -56,10 +55,9 @@ class Posts(db.Model):
             'body': self.body,
             'date': self.date,
             'image_url': self.image_url,
+            'likes': len(self.likes),
+            'comments': [comment.serialize() for comment in self.comments]
         }
-
-
-
 
 class Likes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -67,8 +65,8 @@ class Likes(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=True)
     game_id = db.Column(db.Integer, db.ForeignKey('games.id'), nullable=True)
     user_to = db.relationship('Users', foreign_keys=[user_id])
-    post_to = db.relationship('Posts', foreign_keys=[post_id])
-    game_to = db.relationship('Games', foreign_keys=[game_id])
+    post_to = db.relationship('Posts', backref='likes', foreign_keys=[post_id])
+    game_to = db.relationship('Games', backref='likes', foreign_keys=[game_id])
 
     def __repr__(self):
         return f'<Like {self.id}>'
@@ -81,7 +79,6 @@ class Likes(db.Model):
             'game_id': self.game_id
         }
 
-
 class Games(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), unique=True, nullable=False)
@@ -90,7 +87,6 @@ class Games(db.Model):
     released_at = db.Column(db.String(), unique=False, nullable=True)
     metacritic = db.Column(db.Integer(), unique=False, nullable=True)
 
-
     def __repr__(self):
         return f'<Game {self.name}>'
     
@@ -98,13 +94,16 @@ class Games(db.Model):
         return delete_html_tags(self.description)
         
     def serialize(self):
-        return {'id': self.id,
-                'name': self.name,
-                'background_image': self.background_image, 
-                'description': self.clean_description(),
-                'metacritic': self.metacritic,
-                'released_at': self.released_at}
-
+        return {
+            'id': self.id,
+            'name': self.name,
+            'background_image': self.background_image, 
+            'description': self.clean_description(),
+            'metacritic': self.metacritic,
+            'released_at': self.released_at,
+            'likes': len(self.likes),
+            'comments': [comment.serialize() for comment in self.comments]
+        }
 
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -137,7 +136,6 @@ class Videos(db.Model):
     title = db.Column(db.String(), nullable=True)
     game_name = db.Column(db.String(), nullable=True)
 
-
     def __repr__(self):
         return f'<Video {self.title}>'
 
@@ -148,4 +146,3 @@ class Videos(db.Model):
             'embed': self.embed,
             'game_name': self.game_name
         }
-
