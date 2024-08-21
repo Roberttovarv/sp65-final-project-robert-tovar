4f8c7e4232c8619aae8122be5bc5dec9a189395c
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-08-21 10:58:14 +0200

    Create Arrow.jsx

---
+++ src/front/js/component/Arrow.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+export const Arrow = () => {
+    const navigate = useNavigate();
+
+	return (
+		<div className="text-center mt-5 text-white">
+			<i 
+            class="fa-solid fa-arrow-left" 
+            onClick={navigate(-1)}
+            style={{cursor: "pointer"}}></i>
+		</div>
+	);
+};
